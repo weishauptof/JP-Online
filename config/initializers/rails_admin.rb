8a40c49746cc77993107c0890d9d76e8fdb46c07
shino30001 <shinonono30001@gmail.com>
2021-01-06 23:27:58 +0900

    admin path

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,7 +1,7 @@
 RailsAdmin.config do |config|
   config.authenticate_with do
     authenticate_or_request_with_http_basic('Site Message') do |username, password|
-      username == 'foo' && password == 'bar'
+      username == ENV['ADMIN_ID'] && password == ['ADMIN_PASSWORD']
     end
   end
 # 省略
