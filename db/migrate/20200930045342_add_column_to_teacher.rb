0055a8e9e10e314e8004f0be39e5f2224fb08c93
shino30001 <shinonono30001@gmail.com>
2021-01-14 14:02:33 +0900

    to heroku1

--- db/migrate/20200930045342_add_column_to_teacher.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToTeacher < ActiveRecord::Migration[6.0]
-  def change
-    add_column :teachers, :role, :string, default: "teacher"
-  end
-end
--- db/migrate/20210112030341_delete_teacher.rb
+++
@@ -1,5 +0,0 @@
-class DeleteTeacher < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :Teachers
-  end
-end
