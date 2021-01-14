f6bd3364e7e25487f6642e8b60cca36c2995ed3f
shino30001 <shinonono30001@gmail.com>
2021-01-14 14:12:49 +0900

    to heroku2

--- db/migrate/20210112050627_change_column_feedback.rb
+++ db/migrate/20210112050627_change_column_feedback.rb
@@ -1,6 +1,6 @@
 class ChangeColumnFeedback < ActiveRecord::Migration[6.0]
   def change
     add_column :feedbacks, :a, :integer
-    change_column :feedbacks, :answer, :integer
+    change_column :feedbacks, :answer, 'integer USING CAST(answer AS integer)'
   end
 end
